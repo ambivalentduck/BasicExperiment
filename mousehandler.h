4d654bcc7bc2f921a29f88ab742d769b4dc0ea43
Justin Horowitz <justin.horowitz@gmail.com>
2010-05-13 16:03:53 -0500

    HAPI Handler Implementation begun, opening jitter resolved.

--- mousehandler.h
+++ mousehandler.h
@@ -1,36 +1,37 @@
+#ifndef MOUSEHANDLER_H
+#define MOUSEHANDLER_H
+
 #include <X11/Xlib.h>
-#include "point.h"
 #include <time.h>
+#include <QRect>
+#include "point.h"
 #include "timestuff.h"
 #include "devicehandler.h"
 
 class MouseHandler: public DeviceHandler
 {
 public:
-	MouseHandler(point Center, double Radius)
+	MouseHandler(point Center, double Radius, QRect qr): DeviceHander(Center, Radius) 
 	{
 		d = XOpenDisplay(NULL);
-		w2=W2;
-		h2=H2;
-		home = Point<double>(w2,h2);
-		last = home;
+		last = center;
+		home=point(double(qr.width())/2l-.05, double(qr.height())/2l-.05);
 		XWarpPointer(d,RootWindow(d,DefaultScreen(d)),RootWindow(d,DefaultScreen(d)),0,0,0,0,home.X(),home.Y());
-		offset=Point<double>(home.X()-w2+0.5,h2-home.Y()-0.5);
-		clock_gettime(CLOCK_REALTIME,&t1);
+		offset=point();
+		t1=getTime();
 		t2=t1;
 		rate=120;
-		baseT=1.0/120.0;
-		deviceState state;
+		baseT=1l/rate;
 		state.appliedforce=point();
 	}
 	
 	Point<double> acquire() 
 	{
 		XQueryPointer(d,RootWindow(d,DefaultScreen(d)),&e.xbutton.root,&e.xbutton.window,&e.xbutton.x_root,&e.xbutton.y_root, &e.xbutton.x,&e.xbutton.y,&e.xbutton.state); 
-		current = Point<double>(e.xbutton.x_root-last.X(),last.Y()-e.xbutton.y_root);
-		last=Point<double>(e.xbutton.x_root,e.xbutton.y_root);
+		current = point(e.xbutton.x_root-last.X(),last.Y()-e.xbutton.y_root);
+		last=point(e.xbutton.x_root,e.xbutton.y_root);
 		offset+=current;
-		clock_gettime(CLOCK_REALTIME,&t2);
+		t2=getTime();
 		if(timespecDiffToSec(t1, t2)>0.05) //Works out to 6 samples?
 		{
 			t1=t2;
@@ -50,4 +51,7 @@ private:
 	int w2, h2;
 	Point<double> offset, last, home, current;
 	timespec t1, t2;
+	deviceState state;
 };
+
+#endif
